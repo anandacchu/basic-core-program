bb5ff49ae7318af23ad96aa1502ca64286f53d79
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-08-20 05:55:14 +0530

    uc6

---
+++ Program (1) (1).cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace BasicCoreProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            QuotientRemainder.CalculateQuotientRemainder();
+        }
+    }
+}
